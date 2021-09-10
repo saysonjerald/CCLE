5cce9cc203b7a7ca2292e002c9096002b2df9d50
Jerald Sayson <saysonjerald@gmail.com>
2021-09-11 04:15:59 +0800

    Fix: Removed reack-cookie provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
